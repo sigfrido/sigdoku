422be6482b301e3730c4299663187b3c656853d5
Luca Sigfrido Percich <sigfrido@tiscali.it>
2012-08-21 17:52:10 +0200

    Moved console tests to test_console.py. Added alltests.py

---
+++ sudoku/alltests.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python
+
+from test_console import *
+from test_sudoku import *
+
+unittest.main()
+
+
--- sudoku/test_sudoku.py
+++ sudoku/test_sudoku.py
@@ -1,4 +1,4 @@
-import sudoku, console
+import sudoku
 import unittest
 
 
@@ -140,33 +140,6 @@ class testBoard(unittest.TestCase):
         self.check_row_col_square(80, 9, 8, 9, 8)
         
 
-
-
-class testConsole(unittest.TestCase):
-    
-    
-    def setUp(self):
-        self.console = console.Console(3)
-    
-        
-    def test_console_parse_cmd_quit(self):
-        self.assertTrue(self.console.do_play)
-        self.console.parse_command_line('   q ')
-        self.assertFalse(self.console.do_play)
-        
-        
-    def test_console_parse_move(self):
-        self.console.parse_command_line(' 2  5  8 ')
-        self.assertEqual(8, self.console.board.row(2).cell(5).value)
-
-        self.console.parse_command_line('8 9 1 ')
-        self.assertEqual(1, self.console.board.row(8).cell(9).value)
-        
-        self.console.clear_error()
-        self.console.parse_command_line('8 a 19')
-        self.assertNotEqual('', self.console.error_message)
-
-    
 if __name__ == '__main__':
     unittest.main()
 
